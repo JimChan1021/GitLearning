c138a9e94660a4fb735e45174d8828e563854949
JimChan <jimchan1021@foxmail.com>
2017-12-11 16:13:46 +0800

    创建工程
    创建工程

---
+++ GitLearningTest/GitLearningTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitLearningTest
+//
+//  Created by Jim Chan on 2017/12/11.
+//  Copyright © 2017年 JimChan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
